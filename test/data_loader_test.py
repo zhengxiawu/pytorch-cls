69e3ecc8101eae2748ed55df010f4d8b476f668e
zhengxiawu <zhengxiawu@126.com>
2020-06-24 16:59:15 +0800

    bug fixing

--- test/data_loader_test.py
+++ test/data_loader_test.py
@@ -16,7 +16,7 @@ from pytorch_cls.core.config import cfg
 
 def main():
     config.load_cfg_fom_args("Compute model and loader timings.")
-    config.assert_and_infer_cfg()
+    # config.assert_and_infer_cfg()
     train_loader = loader.construct_train_loader()
     benchmark.compute_time_loader(train_loader)
 
