33aec96573fdf398ad92628b96755617d1b7c831
zhengxiawu <zhengxiawu@126.com>
2020-06-25 16:03:30 +0800

    bug fixing

--- script/data_to_memory.sh
+++ script/data_to_memory.sh
@@ -7,7 +7,7 @@ tar xvf /gdata/ImageNet2012/ILSVRC2012_img_train.tar -C /userhome/temp_data/Imag
 cp /userhome/project/pytorch-cls/script/unzip_image_net.sh /userhome/temp_data/ImageNet2012/train/
 cd /userhome/temp_data/ImageNet2012/train/
 ./unzip_image_net.sh
-cp -r /userhome/ILSVRC2012_img_val/ /userhome/temp_data/ImageNet2012/val
+cp -r /gdata/ImageNet2012/val/ /userhome/temp_data/ImageNet2012/val
 elif [ "$1" == 'cifar10' ];
 then
 mount -t tmpfs -o size=1G tmpfs /userhome/temp_data
